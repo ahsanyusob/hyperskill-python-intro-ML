b75fe20607f0bd061e2c03ed323ec5a3994bc282
AHSAN YUSOB <93210261+ahsanyusob@users.noreply.github.com>
2022-08-02 09:26:39 +0200

    Update regression.py
    - change dataset (loaded from new csv file)
    - add regression model from sklearn
    - compare sklearn regression model with custom one by calculating their differences

--- challenging/Linear Regression From Scratch/regression.py
+++ challenging/Linear Regression From Scratch/regression.py
@@ -1,6 +1,8 @@
 import math
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 class CustomLinearRegression:
@@ -12,77 +14,95 @@ class CustomLinearRegression:
 
     def fit(self, x, y):
         if self.fit_intercept:
-            X = np.array([[1.0, x['Capacity'][i], x['Age'][i]] for i in range(len(x))])  # create nx3-(sys matrix)
-            Y = np.array([[y[i]] for i in range(len(y))])  # create nx1-(output matrix)
-            beta = np.linalg.inv(X.T @ X) @ X.T @ Y  # 3xn x nx3 x 3xn x nx1 = 3x1
-            self.intercept = beta[0, 0]
-            self.coefficient = beta[1:]
+            x.insert(0, 'ones', np.ones(shape=(len(x), 1)), False)  # allow_duplicates = False
+            X = np.array(x)  # create mxn-(system matrix) where m = number of columns and n = number of rows
+            Y = np.array(y)  # create nx1-(output matrix)
+            beta = np.linalg.inv(X.T @ X) @ X.T @ Y  # nxm x mxn x nxm x mx1 = nx1
+            self.intercept = beta[0]
+            self.coefficient = beta.T
         else:
-            X = np.array([[x['Capacity'][i], x['Age'][i]] for i in range(len(x))])  # create nx2-(system matrix)
-            Y = np.array([[y[i]] for i in range(len(y))])  # create nx1-(output matrix)
-            beta = np.linalg.inv(X.T @ X) @ X.T @ Y  # 2xn x nx2 x 2xn x nx1 = 2x1
-            self.intercept = None
-            self.coefficient = beta
-        return self.intercept, self.coefficient, beta
+            X = np.array(x)  # create mxn-(system matrix) where m = number of columns and n = number of rows
+            Y = np.array(y)  # create nx1-(output matrix)
+            beta = np.linalg.inv(X.T @ X) @ X.T @ Y  # nxm x mxn x nxm x mx1 = nx1
+            self.intercept = 0.0
+            self.coefficient = beta.T
+        return beta
 
     def predict(self, x, beta):
         if self.fit_intercept:
-            X = np.array([[1.0, x['Capacity'][i], x['Age'][i]] for i in range(len(x))])  # create nx3-(sys matrix)
+            try:
+                x.insert(0, 'ones', np.ones(shape=(len(x), 1)), False)  # allow_duplicates = False
+            except ValueError:
+                pass
+            X = np.array(x)  # create mxn-(system matrix) where m = number of columns and n = number of rows
         else:
-            X = np.array([[x['Capacity'][i], x['Age'][i]] for i in range(len(x))])  # create nx2-(system matrix)
+            X = np.array(x)  # create mxn-(system matrix) where m = number of columns and n = number of rows
         return X @ beta
 
     def r2_score(self, y, y_hat):
-        Y = np.array([[y[i]] for i in range(len(y))])  # create nx1-(output matrix)
+        Y = np.array(y)  # create nx1-(output matrix)
         y_mean = Y.mean()
-        num = 0
-        den = 0
+        squared_error, squared_deviation = 0, 0
         for i in range(len(Y)):
-            num += (Y[i] - y_hat[i]) ** 2
-            den += (Y[i] - y_mean) ** 2
-        res = 1 - num/den
-        return res[0]
+            squared_error += (Y[i] - y_hat[i]) ** 2
+            squared_deviation += (Y[i] - y_mean) ** 2
+        r2 = 1 - squared_error/squared_deviation
+        return r2[0]
 
     def rmse(self, y, y_hat):
-        Y = np.array([[y[i]] for i in range(len(y))])  # create nx1-(output matrix)
-        square_diff = 0
+        Y = np.array(y)  # create nx1-(output matrix)
+        squared_error = 0
         for i in range(len(Y)):
-            square_diff += (Y[i] - y_hat[i]) ** 2
-        res = (1/len(Y)) * square_diff
-        res = math.sqrt(res[0])
-        return res
+            squared_error += (Y[i] - y_hat[i]) ** 2
+        mse = (1/len(Y)) * squared_error
+        rmse = math.sqrt(mse[0])
+        return rmse
 
 
 def main():
-    # Data x and y
-    capacity = [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9]
-    age = [11, 11, 9, 8, 7, 7, 6, 5, 5, 4]
-    cost_per_ton = [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69]
-
     # Intercept exist?
     intercept_flag = True
 
     # 1 - LOAD a pandas DataFrame containing x and y
-    data_frame = pd.DataFrame(zip(capacity, age, cost_per_ton), columns=['Capacity', 'Age', 'Cost/ton'])
+    # Data x and y
+    # f1 = [2.31, 7.07, 7.07, 2.18, 2.18, 2.18, 7.87, 7.87, 7.87, 7.87]
+    # f2 = [65.2, 78.9, 61.1, 45.8, 54.2, 58.7, 96.1, 100.0, 85.9, 94.3]
+    # f3 = [15.3, 17.8, 17.8, 18.7, 18.7, 18.7, 15.2, 15.2, 15.2, 15.2]
+    # y = [24.0, 21.6, 34.7, 33.4, 36.2, 28.7, 27.1, 16.5, 18.9, 15.0]
+    # dataframe_x = pd.DataFrame(zip(f1, f2, f3), columns=['f1', 'f2', 'f3'])
+    # dataframe_y = pd.DataFrame(y, columns=['y'])
+
+    # Or read from CSV
+    dataframe_x = pd.read_csv('data_stage4.csv', usecols=[0, 1, 2])
+    dataframe_y = pd.read_csv('data_stage4.csv', usecols=[3])
 
     # 2 - INITIALIZE CustomLinearRegression class
-    linear_regression_model = CustomLinearRegression(fit_intercept=intercept_flag)
+    custom_regression_model = CustomLinearRegression(fit_intercept=intercept_flag)
+    scikit_regression_model = LinearRegression(fit_intercept=intercept_flag)
 
-    # 3 - Implement the fit() method
-    intercept, coefficients, beta_hat = linear_regression_model.fit(data_frame[['Capacity', 'Age']],
-                                                                    data_frame['Cost/ton'])
+    # 3 - Fit the data by passing the X DataFrame and y Series to LinearRegression and CustomLinearRegression
+    beta_hat = custom_regression_model.fit(dataframe_x, dataframe_y)
+    scikit_regression_model.fit(dataframe_x, dataframe_y)
+    intercept_crm = custom_regression_model.intercept[0]
+    coefficients_crm = custom_regression_model.coefficient[0][1:]
+    intercept_srm = scikit_regression_model.intercept_[0]
+    coefficients_srm = scikit_regression_model.coef_[0][1:]
 
     # 4 - Predict y for the other dataset xwz and print the result (yhat)
-    y_predicted = linear_regression_model.predict(data_frame[['Capacity', 'Age']], beta_hat)
-    # print(y_predicted)
+    y_predicted_crm = custom_regression_model.predict(dataframe_x, beta_hat)
+    y_predicted_srm = scikit_regression_model.predict(dataframe_x)
 
     # 5 - Calculate RMSE & R2 metrics
-    r2_metrics = linear_regression_model.r2_score(data_frame['Cost/ton'], y_predicted)
-    rmse_metrics = linear_regression_model.rmse(data_frame['Cost/ton'], y_predicted)
+    r2_score_crm = custom_regression_model.r2_score(dataframe_y, y_predicted_crm)
+    r2_score_srm = r2_score(dataframe_y, y_predicted_srm)
+    rmse_crm = custom_regression_model.rmse(dataframe_y, y_predicted_crm)
+    rmse_srm = math.sqrt(mean_squared_error(dataframe_y, y_predicted_srm))
 
-    # 6 - Print the intercept, coefficient, RMSE, and R2 values as a Python dictionary
-    result_dict = {'Intercept': intercept, 'Coefficient': coefficients, 'R2': r2_metrics, 'RMSE': rmse_metrics}
-    print(result_dict)
+    # 6 - Print the differences between intercept, coefficient, RMSE, and R2 values of
+    # LinearRegression and CustomLinearRegression as a Python dictionary
+    result_dictionary = {'Intercept': intercept_srm - intercept_crm, 'Coefficient': coefficients_srm - coefficients_crm,
+                         'R2': r2_score_srm - r2_score_crm, 'RMSE': rmse_srm - rmse_crm}
+    print(result_dictionary)
 
 
 if __name__ == '__main__':
