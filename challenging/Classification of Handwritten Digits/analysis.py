515e4a77d4221f9ef4f203ff0764f5c7b2874770
AHSAN YUSOB <93210261+ahsanyusob@users.noreply.github.com>
2022-08-08 16:38:55 +0200

    Update analysis.py
    Stage 2:
    + split using sklearn train_test_split
    + print new shapes and proportion of sample per class in the training set

--- challenging/Classification of Handwritten Digits/analysis.py
+++ challenging/Classification of Handwritten Digits/analysis.py
@@ -1,9 +1,18 @@
+import pandas as pd
 import tensorflow as tf
 import numpy as np
+import random
+from sklearn.model_selection import train_test_split
 
 
 def main():
     """
+    Objective Stage 2:
+    - 1 Import necessary tool from sklearn to split datasets
+    - 2 Use the first 6000 rows of the dataset.
+      Set the test_set_size = 0.3 & random_seed = 40
+    - 3 Print new data shapes and the proportions of samples per class in the training
+      set
     Objective Stage 1:
     - 1 Import tensorflow and numpy
     - 2 Load MNIST data from tensorflow.keras.datasets
@@ -14,18 +23,35 @@ def main():
       and the shape of the target variable in following format: (n, m).
       Print the min and max values of the features array.
     """
-    # 2 - Load MNIST datasets from tensorflow.keras.datasets
+    # Model settings
+    rows_limit = 6000
+    seed = 40
+    test_size = 0.3  # 30% of dataset
+
+    # Load MNIST datasets from tensorflow.keras.datasets
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    # 3 - Flatten features array with n images and m pixels/image
-    n = np.shape(x_train)[0]
-    m = np.shape(x_train)[1] * np.shape(x_train)[2]
-    x_train = x_train.reshape(n, m)
-    # 4 - Print unique target classes' names, features and target shapes,
-    # and min & max values of the features array
-    print("Classes:", np.unique(y_train))
-    print("Features' shape:", np.shape(x_train))
-    print("Target's shape:", np.shape(y_train))
-    print(f"min: {x_train.min()}, max: {x_train.max()}")
+    X = np.concatenate((x_train, x_test))  # combined features array
+    Y = np.concatenate((y_train, y_test))  # combined target array
+
+    # Flatten features array with n images and m pixels/image
+    n = np.shape(X)[0]
+    m = np.shape(X)[1] * np.shape(X)[2]
+    X = X.reshape(n, m)
+
+    # 2a - Use the first 6000 rows of the dataset
+    # 2b - Set the test_size = 0.3 & random_seed 40
+    X = X[:rows_limit]
+    Y = Y[:rows_limit]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+    # 3 - Print shapes of new data and the proportions of sample per class in the training set
+    # print("Classes:", np.unique(y_train))
+    print("x_train shape:", np.shape(X_train))
+    print("x_test shape:", np.shape(X_test))
+    print("y_train shape:", np.shape(Y_train))
+    print("y_test shape:", np.shape(Y_test))
+    print("Proportion of samples per class in train set:")
+    print(pd.Series(Y_train).value_counts(normalize=True))
 
 
 if __name__ == '__main__':
